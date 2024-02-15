4ed5cf2419ec938a23598fdd08686f2d8b9a2ed9
Sasha Borisov <sb.2004.bor@gmail.com>
2024-02-15 21:06:44 +0300

    Update JoinedToast.kt

--- app/src/main/java/com/topic3/android/reddit/components/JoinedToast.kt
+++ app/src/main/java/com/topic3/android/reddit/components/JoinedToast.kt
@@ -4,9 +4,6 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
-
-package com.topic3.android.reddit.components
-
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.background
